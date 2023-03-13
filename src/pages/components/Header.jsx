0c7967e1a3e353e9e3a1d83a3e552a8ed2d640db
Dmitrii <jumperwf@gmail.com>
2023-03-13 14:22:20 +0200

    comm3

--- src/pages/components/Header.jsx
+++ src/pages/components/Header.jsx
@@ -10,6 +10,8 @@ function Header() {
         <div className="position"> 
             <p className='pHeader'>Клинический психолог, супервизор, RMDR-терапевт, </p>
             <p className='pHeader'>эмоционально-фокусированный терапевт</p>
+            <br />
+            <p className='pHeader'><a href="tel:+79262064197" className='pHeaderPhone'>+7 (926) 206-41-97</a></p>
         </div>
     </div>
   );
