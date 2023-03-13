7da21d1853935a57f18c313bf2a204d7667cb884
Dmitrii <jumperwf@gmail.com>
2023-03-13 17:14:16 +0200

    comm4

--- src/pages/components/Header.jsx
+++ src/pages/components/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
         <div className="block-avatar"><a href="/"><img className='avatar' src={require("./img/avatar.jpg")} alt="Victorya Plesser" /></a></div>
         <div className="name"><h1>Виктория Плессер</h1></div>
         <div className="position"> 
-            <p className='pHeader'>Клинический психолог, супервизор, RMDR-терапевт, </p>
+            <p className='pHeader'>Клинический психолог, супервизор, EMDR-терапевт, </p>
             <p className='pHeader'>эмоционально-фокусированный терапевт</p>
             <br />
             <p className='pHeader'><a href="tel:+79262064197" className='pHeaderPhone'>+7 (926) 206-41-97</a></p>
