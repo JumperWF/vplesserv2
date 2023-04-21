8a2567ed5b2efd3c4fc1e3c1b76c88bdd1977826
Dmitrii <jumperwf@gmail.com>
2023-04-21 14:49:10 +0300

    com6

--- src/pages/components/LegalAsp.jsx
+++ src/pages/components/LegalAsp.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './css/LegalAsp.css';
+import legacy from '../../download/agreements.docx'
 // import legacy from 'https://docs.google.com/document/d/1cgwVimpKDpb_Tou-WAiUfvJUCtsmGvOD/edit?usp=sharing&ouid=100948237123630890808&rtpof=true&sd=true'
 
 function LegalAsp() {
@@ -8,7 +9,7 @@ function LegalAsp() {
 
                 <p>
 
-                    Форма информированного согласия <a href='https://docs.google.com/document/d/1cgwVimpKDpb_Tou-WAiUfvJUCtsmGvOD/edit?usp=sharing&ouid=100948237123630890808&rtpof=true&sd=true' title="согласие.docx" target="_blank" rel="noreferrer">посмотреть файл на google drive</a>
+                    Форма информированного согласия: <button class="download-btn"><a href={legacy} download>Скачать</a></button>
                 
                 </p>
 
--- src/pages/components/Social-icons.jsx
+++ src/pages/components/Social-icons.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import './css/Social-icons.css';
 import tg from './img/icon/tg.svg'
-import vk from './img/icon/vk.svg'
+// import vk from './img/icon/vk.svg'
 import zen from './img/icon/zen.svg'
+import youtube from './img/icon/youtube.svg'
 
 
 function SocialIcons() {
@@ -13,31 +14,33 @@ function SocialIcons() {
         <div className="social-icons">
             <div className="telegram-href">
                 <a href="https://t.me/dom_viol" target="_blank" rel="noreferrer">
-                    {/* <img src={require("./img/icon/tg.png")} alt="telegram" width="40px" height="40px"></img> */}
                     <img src={tg} alt="telegram" width="40px" height="40px"></img>
                 </a>
             </div>
 
             <div className="telegram-href">
                 <a href="https://t.me/selfregulat" target="_blank" rel="noreferrer">
-                    {/* <img src={require("./img/icon/tg.png")} alt="telegram" width="40px" height="40px"></img> */}
                     <img src={tg} alt="telegram" width="40px" height="40px"></img>
                 </a>
             </div>
 
             <div className="zen-href">
                 <a href="https://dzen.ru/vikspsy" target="_blank" rel="noreferrer">
-                    {/* <img src={require("./img/icon/zen.png")} alt="yandex dzen" width="40px" height="40px"></img> */}
                     <img src={zen} alt="yandex dzen" width="40px" height="40px"></img>
                 </a>
             </div>
 
-            <div className="vk-href">
-                <a href="https://vk.com/vikspsy" target="_blank" rel="noreferrer">
-                    {/* <img src={require("./img/icon/vk.png")} alt="vk" width="40px" height="40px"></img> */}
-                    <img src={vk} alt="vk" width="40px" height="40px"></img>
+            <div className="youtube-href">
+                <a href="https://www.youtube.com/@Vikspsy" target="_blank" rel="noreferrer">
+                    <img src={youtube} alt="youtube" width="40px" height="40px"></img>
                 </a>
             </div>
+
+            {/* <div className="vk-href">
+                <a href="https://vk.com/vikspsy" target="_blank" rel="noreferrer">
+                    <img src={vk} alt="vk" width="40px" height="40px"></img>
+                </a>
+            </div> */}
 
             {/* <div className="watsapp-href">
                 <a href="https://wa.me/00000000" target="_blank" rel="noreferrer">
